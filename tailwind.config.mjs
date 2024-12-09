6b2d019b284643ecaad1c27f515666f3d49bf454
hid3h <razyetts@hotmail.co.jp>
2024-12-09 11:59:48 +0900

    wip

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#FAF9F6",
+        foreground: "#242424",
+        accent: {
+          DEFAULT: "#1A8917",
+          foreground: "#FFFFFF",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
